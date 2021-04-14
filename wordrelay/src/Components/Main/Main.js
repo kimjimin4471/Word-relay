c9087fbb6fa3db5b86dec36b7246d4d5fe89f82f
Kimjimin0807 <jm004471@naver.com>
2021-04-14 20:28:25 +0900

    타임오버 추가

--- wordrelay/src/Components/Main/Main.js
+++ wordrelay/src/Components/Main/Main.js
@@ -1,15 +1,19 @@
-import React, { useState, memo, useRef } from 'react';
+import React, { useState, memo, useRef, useEffect } from 'react';
 import Words from './words/Words';
 import * as s from './style';
+
+const blank_pattern = /[\s]/g;
 
 const word = new Array('개나리', '복숭아', '사과', '바나나', '딸기');
 
 const Main = memo(() => {
   const [baseWord, setBaseWord] = useState(word[Math.floor(Math.random()*word.length)]);
   const [userWord, setUserWord] = useState('');
-  // const [wrongAnswer, setWrongAnswer] = useState(false);
+  const [message, setMessage] = useState('');
+  const [gameOver, setGameOver] = useState(false);
   const [words, setWords] = useState([]);
   const fontAnim = useRef(null);
+  const gameTimer = useRef(null);
 
   const onChangeInput = (e) => {
     setUserWord(e.target.value);
@@ -17,6 +21,11 @@ const Main = memo(() => {
 
   const onSubmitWord = (e) => {
     e.preventDefault();
+    if(blank_pattern.test(userWord) == true){
+      setMessage('공백 입력!!');
+      setUserWord('');
+      return;
+    }
     if(userWord[0] == baseWord[baseWord.length-1]){
       setUserWord('');
       setWords([
@@ -27,27 +36,40 @@ const Main = memo(() => {
     }
     else {
       alert("실패!");
-      window.location.reload();
-      // setWrongAnswer(true);
+      setGameOver(true);
     }
   }
 
-  let timer = 0;
+  useEffect(() => {
+    let timer = 0;
 
-  const test = () => {
-    setInterval(() => {
+    fontAnim.current.style.fontSize = `${20}vmin`;
+
+    clearInterval(gameTimer.current);
+
+    gameTimer.current = setInterval( ()=> {
       if(!fontAnim.current){
         return;
       }
-      timer += 1;
-      fontAnim.current.style.fontSize = `${20-timer/2}vmin`;
-      
-    }, 500);
-  }
 
-  test();
+      if((20-timer) <= 0) {
+        setMessage('타임오버!');
+        clearInterval(gameTimer.current);
+        setGameOver(true);
+      }
+
+      timer += 1;
+      fontAnim.current.style.fontSize = `${20-timer}vmin`;
+    }, 1000);
+
+  }, [baseWord]);
+
   return(
     <>
+      {gameOver ? <div>
+        <s.WhiteBox></s.WhiteBox>
+        
+      </div> : null}
       <s.FirstWord ref={fontAnim}>{baseWord}</s.FirstWord>
       <div>
         {words.map((v, i) => {
@@ -56,6 +78,7 @@ const Main = memo(() => {
           );
         })}
       </div>
+      <s.WrongAnswer>{message}</s.WrongAnswer>
       <form onSubmit={onSubmitWord}>
         <s.UserWordInput onChange={onChangeInput} value={userWord}></s.UserWordInput>
       </form>
--- wordrelay/src/Components/Main/style.js
+++ wordrelay/src/Components/Main/style.js
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
 
-export const FirstWord = styled.h1`
+export const FirstWord = styled.p`
   font-weight: bold;
   margin: 0;
   font-size: 20vmin;
-  transition: 1s;
+  transition: 2s;
 `
 
 export const WrongAnswer = styled.p`
+  color: red;
 `
 
 export const UserWordInput = styled.input`
   text-align: center;
+`
+
+export const WhiteBox = styled.div`
+  
 `
